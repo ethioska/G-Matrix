c8369fcfbd8870ac3ad0042d90cfcc0d2dddd7d8
ethioska <ethioska@gmail.com>
2025-09-03 11:07:52 +0300

    Create config.js

---
+++ bot/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "8403746831:AAFq8POLVZHhlm1ifnsp2i4Qq4G-63_wuZQ",   // put your bot token here
+  ADMIN_ID: 6905441721,                    // your Telegram user ID (only you can post)
+  NEWS_FILE: "../webapp/news.json"        // relative path to webapp/news.json
+};
