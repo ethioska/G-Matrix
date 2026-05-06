c9dd11922435ddd13179315fc8f44b2ff7b62e24
ethioska <ethioska@gmail.com>
2026-05-06 16:27:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
-app.listen(PORT, () => console.log(`Hala Style App running on ${PORT}`));
+app.use(express.static('public'));
+app.listen(process.env.PORT || 3000, () => console.log('Server is running...'));
