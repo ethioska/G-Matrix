608d099916a3407f3ee2134f19abf86ae4b56373
ethioska <ethioska@gmail.com>
2026-05-06 15:15:05 +0300

    Refactor server.js for cleaner syntax and logging

--- server.js
+++ server.js
@@ -4,11 +4,5 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.listen(PORT, () => {
-    console.log(`Voice Party Room is live on port ${PORT}`);
-});
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.listen(PORT, () => console.log(`Hala Style App running on ${PORT}`));
